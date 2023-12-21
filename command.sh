b673851cd8ee46e903d11b26d9b9e63b3a2e92d2
HeavyFeather <90891176+HeavyFeather@users.noreply.github.com>
2023-12-21 23:45:32 +0700

    Add files via upload

---
+++ command.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+read -p "Enter command: " dirname #enter new string
+
+COMM=`ls $dirname` #command
+echo $COMM
+
+
---
+++ update.sh
@@ -0,0 +1,4 @@
+echo "System updating"
+sudo apt update
+sudo apt upgrade
+echo "System updated"
